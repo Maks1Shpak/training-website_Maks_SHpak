af1262bec9eef195e7210eb169a266f98bbfbc48
Maks1Shpak <maksimshpak20047@gmail.com>
2025-05-21 20:15:14 +0000

    FIx title

---
+++ pages/population.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PopulationPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Поширення зайців</h1>
+      {/* ...existing content... */}
+    </div>
+  );
+};
+
+export default PopulationPage;
